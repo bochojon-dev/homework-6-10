c1dfc84a3af8956201c109b79d9e88e96618a8bf
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-04-29 09:55:09 +0500

    hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "../hero/Hero.css";
+import hero from "../../assets/hero-image.jpg";
+
+export class Hero extends Component {
+  render() {
+    return (
+      <div className="hero">
+        <div className="container">
+          <div className="hero_contents">
+            <div className="hero_texts">
+              <h3>Making time a good time by making food the good food.</h3>
+              <p>
+                There are many things are needed to start the Fast Food
+                Business. You need not only Just Food Stalls with Persons but
+                also specialized equipment,
+              </p>
+              <div className="buttons">
+                <button className="btn1">Order Now</button>
+                <button className="btn2">Food Details</button>
+              </div>
+            </div>
+            <img src={hero} alt="hero img" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Hero;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import logo from "../../assets/logo.svg";
+import "../navbar/Navbar.css";
 
+const links = ["Home", "About Us", "Menu", "Features", "Contact Us"];
+const navLink = links?.map((e, i) => <li key={i}>{e} </li>);
 export class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
         <div className="container">
           <div className="navbar_contents">
-            <img src={logo} alt="logo" />
+            <div className="logo_links">
+              <img src={logo} alt="logo" />
+              <ul>{navLink}</ul>
+            </div>
+            <div className="book">
+              <button>Booking Now</button>
+            </div>
           </div>
         </div>
       </div>
