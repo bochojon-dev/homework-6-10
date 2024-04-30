a8f8105a61c8a0c6b9a0ffd01e0924a57c788fdf
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-04-30 14:56:18 +0500

    bugs fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,99 @@
 import React, { Component } from "react";
 import "../footer/Footer.css";
+import "../footer/Footer.css";
+import logo from "../../assets/footer-logo.png";
+import instagram from "../../assets/instagram.svg";
+import facebook from "../../assets/facebook.svg";
+import twitter from "../../assets/twitter.svg";
+import youtube from "../../assets/youtube.svg";
 
 export class Footer extends Component {
   render() {
     return (
-      <div>
-        <h2>Footer</h2>
+      // <div>
+      //   <h2>Footer</h2>
+      // </div>
+      <div className="footer">
+        <div className="container">
+          <div className="footer_contents">
+            <ul>
+              <li>
+                <img src={logo} alt="logo" />
+              </li>
+              <li>
+                <p>www.company name.com</p>
+              </li>
+              <li>
+                <p>companyname@gmail.com</p>
+              </li>
+              <li>
+                <p>Phone: +7 485-118-03-25</p>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <h4>Home</h4>
+              </li>
+              <li>
+                <p>Landingpage</p>
+              </li>
+              <li>
+                <p>Documentation</p>
+              </li>
+              <li>
+                <p>Referral Program</p>
+              </li>
+              <li>
+                <p>UI & UX Design</p>
+              </li>
+              <li>
+                <p>Web Design</p>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <h4>Menu</h4>
+              </li>
+              <li>
+                <p>Landingpage</p>
+              </li>
+              <li>
+                <p>Documentation</p>
+              </li>
+              <li>
+                <p>Referral Program</p>
+              </li>
+              <li>
+                <p>UI & UX Design</p>
+              </li>
+              <li>
+                <p>Web Design</p>
+              </li>
+            </ul>
+            <ul>
+              <li>
+                <h4>Company</h4>
+              </li>
+              <li>
+                <p>Landingpage</p>
+              </li>
+              <li>
+                <p>Documentation</p>
+              </li>
+              <li>
+                <p>Referral Program</p>
+              </li>
+              <li>
+                <div className="social_medias">
+                  <img src={instagram} alt="instagram" />
+                  <img src={facebook} alt="facebook" />
+                  <img src={twitter} alt="twitter" />
+                  <img src={youtube} alt="youtube" />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
@@ -25,88 +113,88 @@ export default Footer;
 
 // export class Footer extends Component {
 //   render() {
-//     <div className="footer">
-//       <div className="container">
-//         <div className="footer_contents">
-//           <ul>
-//             <li>
-//               <img src={logo} alt="logo" />
-//             </li>
-//             <li>
-//               <p>www.company name.com</p>
-//             </li>
-//             <li>
-//               <p>companyname@gmail.com</p>
-//             </li>
-//             <li>
-//               <p>companyname@gmail.com</p>
-//             </li>
-//           </ul>
-//           <ul>
-//             <li>
-//               <h4>Home</h4>
-//             </li>
-//             <li>
-//               <p>Landingpage</p>
-//             </li>
-//             <li>
-//               <p>Documentation</p>
-//             </li>
-//             <li>
-//               <p>Referral Program</p>
-//             </li>
-//             <li>
-//               <p>UI & UX Design</p>
-//             </li>
-//             <li>
-//               <p>Web Design</p>
-//             </li>
-//           </ul>
-//           <ul>
-//             <li>
-//               <h4>Menu</h4>
-//             </li>
-//             <li>
-//               <p>Landingpage</p>
-//             </li>
-//             <li>
-//               <p>Documentation</p>
-//             </li>
-//             <li>
-//               <p>Referral Program</p>
-//             </li>
-//             <li>
-//               <p>UI & UX Design</p>
-//             </li>
-//             <li>
-//               <p>Web Design</p>
-//             </li>
-//           </ul>
-//           <ul>
-//             <li>
-//               <h4>Company</h4>
-//             </li>
-//             <li>
-//               <p>Landingpage</p>
-//             </li>
-//             <li>
-//               <p>Documentation</p>
-//             </li>
-//             <li>
-//               <p>Referral Program</p>
-//             </li>
-//             <li>
-//               <div className="social_medias">
-//                 <img src={instagram} alt="instagram" />
-//                 <img src={facebook} alt="facebook" />
-//                 <img src={twitter} alt="twitter" />
-//                 <img src={youtube} alt="youtube" />
-//               </div>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </div>;
+// <div className="footer">
+//   <div className="container">
+//     <div className="footer_contents">
+//       <ul>
+//         <li>
+//           <img src={logo} alt="logo" />
+//         </li>
+//         <li>
+//           <p>www.company name.com</p>
+//         </li>
+//         <li>
+//           <p>companyname@gmail.com</p>
+//         </li>
+//         <li>
+//           <p>companyname@gmail.com</p>
+//         </li>
+//       </ul>
+//       <ul>
+//         <li>
+//           <h4>Home</h4>
+//         </li>
+//         <li>
+//           <p>Landingpage</p>
+//         </li>
+//         <li>
+//           <p>Documentation</p>
+//         </li>
+//         <li>
+//           <p>Referral Program</p>
+//         </li>
+//         <li>
+//           <p>UI & UX Design</p>
+//         </li>
+//         <li>
+//           <p>Web Design</p>
+//         </li>
+//       </ul>
+//       <ul>
+//         <li>
+//           <h4>Menu</h4>
+//         </li>
+//         <li>
+//           <p>Landingpage</p>
+//         </li>
+//         <li>
+//           <p>Documentation</p>
+//         </li>
+//         <li>
+//           <p>Referral Program</p>
+//         </li>
+//         <li>
+//           <p>UI & UX Design</p>
+//         </li>
+//         <li>
+//           <p>Web Design</p>
+//         </li>
+//       </ul>
+//       <ul>
+//         <li>
+//           <h4>Company</h4>
+//         </li>
+//         <li>
+//           <p>Landingpage</p>
+//         </li>
+//         <li>
+//           <p>Documentation</p>
+//         </li>
+//         <li>
+//           <p>Referral Program</p>
+//         </li>
+//         <li>
+//           <div className="social_medias">
+//             <img src={instagram} alt="instagram" />
+//             <img src={facebook} alt="facebook" />
+//             <img src={twitter} alt="twitter" />
+//             <img src={youtube} alt="youtube" />
+//           </div>
+//         </li>
+//       </ul>
+//     </div>
+//   </div>
+// </div>;
 //   }
 // }
 
